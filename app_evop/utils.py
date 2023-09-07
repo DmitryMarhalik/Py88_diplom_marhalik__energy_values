10c5bd70c3f8f469fa28db6b5e29da15221a7e59
DmitryMarhalik <dmitrymarhalik.gmail.com>
2023-09-07 10:46:22 +0300

    del template tags

--- app_evop/utils.py
+++ app_evop/utils.py
@@ -1,3 +1,5 @@
+from app_evop.models import Category
+
 tabs = [{'title': 'Add Food 🍟', 'url_name': 'add_food'},
         {'title': 'Intake 📅', 'url_name': 'intake'},
         {'title': 'Calculation Result 📝', 'url_name': 'calculation_result'},
@@ -5,16 +7,17 @@ tabs = [{'title': 'Add Food 🍟', 'url_name': 'add_food'},
         ]
 
 no_authtabs = [{'title': 'Feedback 📧', 'url_name': 'feedback'}]
-
+categories = Category.objects.all()
 
 class ContextMixin:
 
     def get_user_context(self, **kwargs):
         context = kwargs
+        context['categories'] = categories
         if not self.request.user.is_authenticated:
             context['tabs'] = no_authtabs
         else:
             context['tabs'] = tabs
         if 'cat_selected' not in context:
-            context['cat_selected'] = 0
+            context['cat_selected'] = None
         return context
--- app_evop/views.py
+++ app_evop/views.py
@@ -15,19 +15,20 @@ from django.conf import settings
 
 from app_evop.forms import IntakeForm, AddFoodForm, CalculationResultForm, RegisterUserForm, FeedbackForm
 from app_evop.models import Food, Intake
-from app_evop.utils import ContextMixin, tabs
+from app_evop.utils import ContextMixin, tabs, categories
 
 
 class HomePage(ContextMixin, ListView):
     model = Food  # if not ---> HomePage is missing a QuerySet. Define HomePage.model,
-                  # HomePage.queryset, or override HomePage.get_queryset().
+    # HomePage.queryset, or override HomePage.get_queryset().
     template_name = 'evop/main.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         user_context = self.get_user_context(title='Main page')  # in ContexMixin add title
-                                  # return context(dict) with tabs, title,cat_selected --> add to super().context
+        # return context(dict) with tabs, title,cat_selected --> add to super().context
         # context = dict(list(context.items()) + list(user_context.items())) #or
+        print(user_context)
         context.update(user_context)
         return context
 
@@ -48,9 +49,46 @@ class AllFoods(ContextMixin, ListView):
         return Food.objects.filter(be_confirmed=True)
 
 
+class AddFood(ContextMixin, CreateView):
+    form_class = AddFoodForm
+    template_name = 'evop/add_food.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_context = self.get_user_context(title='Add food')
+        context.update(user_context)
+        return context
+
+    def get_success_url(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        food = context.get('food').name
+        return reverse('success', args=[{'food': food}])
+
+
+class ShowCategory(ContextMixin, ListView):
+    paginate_by = 3
+    # model = Category
+    template_name = 'evop/show_category.html'
+    context_object_name = 'foods'
+    allow_empty = False
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context)
+        user_context = self.get_user_context(title='Category : ' + str(context['foods'][0].category.name),
+                                             cat_selected=context['foods'][0].category.slug)
+        context.update(user_context)
+        return context
+
+    def get_queryset(self):
+        cat_slug = self.kwargs['cat_slug']  # list categories(app_evop_tags)c.get_absolute_url<--(models.kwargs)-basis
+        return Food.objects.filter(category__slug=cat_slug, be_confirmed=True).order_by('-id')
+
+
 class AddIntake(LoginRequiredMixin, ContextMixin, CreateView):
     form_class = IntakeForm
     template_name = 'evop/intake.html'
+
     # success_url = reverse_lazy('success_add_intake')
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -70,43 +108,6 @@ class AddIntake(LoginRequiredMixin, ContextMixin, CreateView):
         return reverse('success', args=[{'intake': intake}])
 
 
-class AddFood(ContextMixin, CreateView):
-    form_class = AddFoodForm
-    template_name = 'evop/add_food.html'
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user_context = self.get_user_context(title='Add food')
-        context.update(user_context)
-        return context
-
-    def get_success_url(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        food = context.get('food').name
-        return reverse('success', args=[{'food': food}])
-
-
-
-class ShowCategory(ContextMixin, ListView):
-    paginate_by = 3
-    # model = Category
-    template_name = 'evop/show_category.html'
-    context_object_name = 'foods'
-    allow_empty = False
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        print(context)
-        user_context = self.get_user_context(title='Category : ' + str(context['foods'][0].category.name),
-                                             cat_selected=context['foods'][0].category.slug)
-        context.update(user_context)
-        return context
-
-    def get_queryset(self):
-        cat_slug = self.kwargs['cat_slug'] # list categories(app_evop_tags)c.get_absolute_url<--(models.kwargs)-basis
-        return Food.objects.filter(category__slug=cat_slug, be_confirmed=True).order_by('-id')
-
-
 class CalculetionResult(ContextMixin, ListView):
     paginate_by = 10
 
@@ -114,7 +115,8 @@ class CalculetionResult(ContextMixin, ListView):
         form = CalculationResultForm
         context = self.get_user_context(title='Calculation')
         return render(request, 'evop/calculation_result.html',
-                      context={'form': form, 'title': context['title'], 'tabs': context['tabs']})
+                      context={'form': form, 'title': context['title'], 'tabs': context['tabs'],
+                               'categories': context['categories']})
 
     def post(self, request, *args, **kwargs):
         context = self.get_user_context(title='Calculation')
@@ -127,7 +129,8 @@ class CalculetionResult(ContextMixin, ListView):
         if not all_intake_product:
             message = 'You have not consumed anything for a given period of time.'
             return render(request, 'evop/final_result.html',
-                          context={'tabs': context['tabs'], 'title': 'No result', 'message': message})
+                          context={'tabs': context['tabs'],  'categories': context['categories'],
+                                   'title': 'No result', 'message': message})
         else:
             count_of_product = dict(
                 Counter(all_intake_product.values_list('food__name')))  # {('chips',): 1,('water',):1}
@@ -142,60 +145,17 @@ class CalculetionResult(ContextMixin, ListView):
             dict_energy_values = {'proteins': round(proteins, 1), 'fats': round(fats, 1),
                                   'carbohydrates': round(carbohydrates, 1), 'kcal': round(kcal, 1)}
             return render(request, 'evop/final_result.html',
-                          context={'tabs': context['tabs'], 'title': 'Final calculation',
+                          context={'tabs': context['tabs'], 'categories': context['categories'],
+                                   'title': 'Final calculation',
                                    'energy_values': dict_energy_values,
                                    'count_product': counts_of_products, 'message': message
                                    })
 
 
-class SignIn(ContextMixin, LoginView):
-    form_class = AuthenticationForm
-    template_name = 'evop/sign_in.html'
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user_context = self.get_user_context(title='Sign In')
-        context.update(user_context)
-        return context
-
-    def get_success_url(self, **kwargs):
-        username = self.request.user.username
-        return reverse('success', args=[{'name': username}])
-        # return redirect('home')
-
-
-
-class SignUp(ContextMixin, CreateView):
-    form_class = RegisterUserForm
-    template_name = 'evop/sign_up.html'
-    # success_url = reverse_lazy('success_registration_user')
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user_context = self.get_user_context(title='Sign Up')
-        context.update(user_context)
-        return context
-
-    def get_success_url(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        username = context.get('user').username
-        return reverse('success', args=[{'reg_user': username}])
-
-    # def form_valid(self, form):                   #для автоматического входа при регистрации
-    #     user=form.save()
-    #     login(self.request, user)
-    #     return redirect('home')
-    #     username=self.request.user.username
-    #     return reverse('success_registration_user',kwargs={'name': username})
-
-def sign_out_user(request):
-    logout(request)
-    return redirect('home')
-
-
 class FeedBack(ContextMixin, FormView):  # Formview не привязано к модели
     form_class = FeedbackForm
     template_name = 'evop/feedback.html'
+
     # success_url = reverse_lazy('success_send_message')
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -228,19 +188,69 @@ class FeedBack(ContextMixin, FormView):  # Formview не привязано к �
         return redirect('success', args={'feedback': name})
 
 
+class SignIn(ContextMixin, LoginView):
+    form_class = AuthenticationForm
+    template_name = 'evop/sign_in.html'
 
-def success(request, args):            # {'name': 'Dima'}
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_context = self.get_user_context(title='Sign In')
+        context.update(user_context)
+        return context
+
+    def get_success_url(self, **kwargs):
+        username = self.request.user.username
+        return reverse('success', args=[{'name': username}])
+        # return redirect('home')
+
+
+class SignUp(ContextMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'evop/sign_up.html'
+
+    # success_url = reverse_lazy('success_registration_user')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_context = self.get_user_context(title='Sign Up')
+        context.update(user_context)
+        return context
+
+    def get_success_url(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = context.get('user').username
+        return reverse('success', args=[{'reg_user': username}])
+
+    # def form_valid(self, form):                   #для автоматического входа при регистрации
+    #     user=form.save()
+    #     login(self.request, user)
+    #     return redirect('home')
+    #     username=self.request.user.username
+    #     return reverse('success_registration_user',kwargs={'name': username})
+
+
+def sign_out_user(request):
+    logout(request)
+    return redirect('home')
+
+
+def success(request, args):  # {'name': 'Dima'}
     arg = args.split(' ').pop()[1:-2]  # 'Dima'}-->Dima
     if 'name' in args:
-        return render(request, 'evop/success.html', {'tabs': tabs, 'username': arg})
+        return render(request, 'evop/success.html', {'tabs': tabs,
+                                                     'username': arg, 'categories': categories})
     elif 'food' in args:
-        return render(request, 'evop/success.html', {'tabs': tabs, 'food': arg})
+        return render(request, 'evop/success.html', {'tabs': tabs,
+                                                     'food': arg, 'categories': categories})
     elif 'feedback' in args:
-        return render(request, 'evop/success.html', {'tabs': tabs, 'feedbackname': arg})
+        return render(request, 'evop/success.html', {'tabs': tabs,
+                                                     'feedbackname': arg, 'categories': categories})
     elif 'reg_user' in args:
-        return render(request, 'evop/success.html', {'tabs': tabs, 'reg_user': arg})
+        return render(request, 'evop/success.html', {'tabs': tabs,
+                                                     'reg_user': arg, 'categories': categories})
     elif 'intake' in args:
-        return render(request, 'evop/success.html', {'tabs': tabs, 'intake': arg})
+        return render(request, 'evop/success.html', {'tabs': tabs,
+                                                     'intake': arg, 'categories': categories})
     else:
         HttpResponse('<h1>Somethink went wrong</h1>')
 
